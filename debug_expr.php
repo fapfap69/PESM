e450e8ef63aecc6e0ca2e9d40138f237dbe1a19a
Antonio Franco <franco.antonio63@gmail.com>
2026-02-03 12:57:29 +0100

    chore: Remove all test and debug files

--- debug_expr.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/src/Parser/GeneratedParser.php';
-
-$parser = new PESM\Parser\GeneratedParser('10+5');
-$result = $parser->match_Expression();
-
-echo json_encode($result, JSON_PRETTY_PRINT);
--- debug_if_exec.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/src/ScriptEngine.php';
-
-$engine = new PESM\ScriptEngine();
-
-$result = $engine->execute('IF 1>0 x=5 END');
-echo "Result:\n";
-print_r($result);
